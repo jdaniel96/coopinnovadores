fdd1f6bb35306b5092d3ad3376f8b697dfa46691
Cristopher Areche <cristopherareche@Cristophers-MacBook-Pro.local>
2022-11-04 09:46:51 -0400

    boostrap library and footer added

---
+++ coopinnovadores/src/components/Footer.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          {/*column 1*/}
+          <div className="col">
+            <h4>CoopInnovadores</h4>
+            <ul className="list-unstyle">
+              <li>Something Here</li>
+              <li>Something Here</li>
+              <li>Something Here</li>
+            </ul>
+          </div>
+          {/*column 2*/}
+          <div className="col">
+            <h4>Ubicacion</h4>
+            <ul className="list-unstyle">
+              <li>Something Here</li>
+              <li>Something Here</li>
+              <li>Something Here</li>
+            </ul>
+          </div>
+          {/*column 3*/}
+          <div className="col">
+            <h4>Contactanos</h4>
+            <ul className="list-unstyle">
+              <li>Something About</li>
+              <li>Something About</li>
+              <li>Something About</li>
+            </ul>
+          </div>
+          {/*column 4*/}
+          <div className="col">
+            <h4>Enlaces</h4>
+            <ul className="list-unstyle">
+              <li>Something About</li>
+              <li>Something About</li>
+              <li>Something About</li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <p className="col-sm">
+            &copy;{new Date().getFullYear()} Coopinnovadores Inc | All rights
+            reserved | Terms of service | Privacy
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
