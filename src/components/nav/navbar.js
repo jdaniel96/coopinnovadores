9e0480081cac504b2a7bf790e405d00ae14d67d0
J.Daniel <90231095+jdaniel96@users.noreply.github.com>
2022-11-24 19:24:32 -0400

    fixed folder names (#21)

--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./navbar.css";
 import React from "react";
 
@@ -64,41 +63,3 @@ function Navbar() {
 }
 
 export default Navbar;
-=======
-import "./navbar.css"
-
-function Navbar (){
-    return (
-        <header className="header">
-            <div className="container">
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">COOPERATIVA</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Acerca</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Contactanos</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Otra Cosa</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </header>
-    );
-}
-
-export default Navbar;
->>>>>>> e69cb0b724e0efaaec3569bbc565f3ea818c5c2b
