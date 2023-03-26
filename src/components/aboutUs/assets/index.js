a8d7fcc3e4643686d211a86a9fe3008d24a5e489
J.Daniel <juandaniel9619@gmail.com>
2023-03-26 09:31:53 -0400

    corrected eslint warning

--- src/components/aboutUs/assets/index.js
+++ src/components/aboutUs/assets/index.js
@@ -1,8 +1,10 @@
-import whoWeAreImg from "./whoWeAre.svg"
-import whyUsImg from "./whyUs.svg"
-import whatWeDo from "./whatWeDo.svg"
-export default {
-    whoWeAreImg: whoWeAreImg,
-    whyUsImg: whyUsImg,
-    whatWeDo: whatWeDo
-}
+import whoWeAreImg from "./whoWeAre.svg";
+import whyUsImg from "./whyUs.svg";
+import whatWeDo from "./whatWeDo.svg";
+const assets = {
+  whoWeAreImg: whoWeAreImg,
+  whyUsImg: whyUsImg,
+  whatWeDo: whatWeDo,
+};
+
+export default assets;
